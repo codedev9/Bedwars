0afb274c2891b85425ed9e73b3cd6529e0c4368f
CodeDev9 <127266511+codedev9@users.noreply.github.com>
2023-07-19 13:55:02 +1200

    Library Update

--- bedwars.lua
+++ bedwars.lua
@@ -4,6 +4,39 @@
 local Bedwars = {}
 local player = game.Players.LocalPlayer
 local Mouse = player:GetMouse()
-local BedwarsLibrary = {}
---// Making the Library
-local function BedwarsLibrary.Get
+local BedwarsLibrary = {
+    ["GUI"] = {
+        ["Combat"]= {
+           ["Aura"] = false
+           ["Sprint"] = false
+        },
+        ["Blatant"] = {
+           ["Fly"] = false
+        },
+        ["Render"] = {
+           ["Cape"] = false
+        },
+        ["World"] = {
+         ["Nuker"] = false
+        },
+        ["Other"] = {
+            ["Private"] = false
+            ["ScriptLoaded"] = false
+        }
+    }
+}
+--// Making the Gui
+local CombatWindow = BedwarsLibrary.GUI.Combat
+local BlatantWindow = BedwarsLibrary.GUI.Blatant
+local RenderWindow = BedwarsLibrary.GUI.Render
+local WorldWindow = BedwarsLibrary.GUI.World
+local shared = BedwarsLibrary.GUI.Other
+--//
+local function Bedwars.loadscript()
+    if shared.Private == true then
+        local gameID = game.PlaceId
+        if gameID == nil then
+            print("BEDWARS | Game Id is nil")
+        end
+    end
+end
--- main.lua
+++ main.lua
@@ -1,7 +1,24 @@
 --// Functions //--
-local function MakeLibrary(Name, IntroEnabled, IntroText)
-
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+--// Create Window
+local BedwarsHub = {}
+local function BedwarsHub.newWindow(name, introEnabled, introText)
+    local Window = OrionLib:MakeWindow({Name = name, HidePremium = false, SaveConfig = true, ConfigFolder = "BedwarsHub"}, IntroEnabled = introEnabled, IntroText = IntroText)
 end
+--//
+local function BedwarsHub.newTab(TabName)
+    local Tab = Window:MakeTab({
+        Name = TabName,
+        Icon = nil,
+        PremiumOnly = false
+    })
+end
+
+local function BedwarsHub.newSection(SectionName)
+   
+end
+return BedwarsHub
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
 local function chatTags(ChatTagName, lplrname)
     local textChatService = game:GetService("TextChatService")
     textChatService.OnIncomingMessage = function(message: TextChatMessage)
