1a8ec5a1cac24afd96fc6c9fa56d64fd1f47434b
CodeDev9 <127266511+codedev9@users.noreply.github.com>
2023-07-20 13:48:00 +1200

    Library Being Reworked

--- main.lua
+++ main.lua
@@ -2,33 +2,12 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 --// Create Window
 local BedwarsHub = {}
-local function BedwarsHub.newWindow(name, introEnabled, introText)
-    local Window = OrionLib:MakeWindow({Name = name, HidePremium = false, SaveConfig = true, ConfigFolder = "BedwarsHub"}, IntroEnabled = introEnabled, IntroText = IntroText)
-end
---//
-local function BedwarsHub.newTab(TabName)
-    local Tab = Window:MakeTab({
-        Name = TabName,
-        Icon = nil,
-        PremiumOnly = false
-    })
-end
-
-local function BedwarsHub.newSection(SectionName)
-   
-end
+OrionLib:MakeNotification({
+    Name = "Important",
+    Content = "This Library is being developed",
+    Image = nil,
+    Time = 3
+})
+game.Players.LocalPlayer:Kick("BedwarsHub Is not Developed")
 return BedwarsHub
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
-local function chatTags(ChatTagName, lplrname)
-    local textChatService = game:GetService("TextChatService")
-    textChatService.OnIncomingMessage = function(message: TextChatMessage)
-        local properties = Instance.new("TextChatMessageProperties")
-        if message.TextSource then
-            local player = game:GetService("Players"):GetPlayerByUserId(message.TextSource.UserId)
-            if player.Name == game.Players.LocalPlayer.Name then
-                properties.PrefixText = "<font color='#00ffee'>[KAVIN OWNER]</font> " .. message.PrefixText
-            end
-        end
-        return properties
-    end
-end
