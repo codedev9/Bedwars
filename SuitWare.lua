17e8e975252f9aa6cd6899e87e3fe0e21c09272e
CodeDev9 <127266511+codedev9@users.noreply.github.com>
2023-08-02 18:30:31 +1200

    hhhhh

--- SuitWare.lua
+++ SuitWare.lua
@@ -2,6 +2,35 @@
     SUITWARE
     Made By suitxyz
 ]]
+local hasTeleported = false
+    local TweenService = game:GetService("TweenService")
+
+    function findNearestBed()
+        local nearestBed = nil
+        local minDistance = math.huge
+
+        for _,v in pairs(game.Workspace:GetDescendants()) do
+            if v.Name:lower() == "bed" and v:FindFirstChild("Covers") and v:FindFirstChild("Covers").BrickColor ~= lplr.Team.TeamColor then
+                local distance = (v.Position - lplr.Character.HumanoidRootPart.Position).magnitude
+                if distance < minDistance then
+                    nearestBed = v
+                    minDistance = distance
+                end
+            end
+        end
+        return nearestBed
+    end
+    function tweenToNearestBed()
+        local nearestBed = findNearestBed()
+        if nearestBed and not hasTeleported then
+            hasTeleported = true
+
+            local tweenInfo = TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0)
+
+            local tween = TweenService:Create(lplr.Character.HumanoidRootPart, TweenInfo.new(0.94), {CFrame = nearestBed.CFrame + Vector3.new(0, 2, 0)})
+            tween:Play()
+        end
+    end
 shared.VapeIndependent = true
 shared.CustomSaveVape = "vape"
 local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
@@ -43,9 +72,9 @@ local Heatseeker = Utility.CreateOptionsButton({
               end)
         end
     end,
-    HoverText = "allows Infnite fly", -- text that will show up after hovering over the button (optional)
-    Default = true, -- enabled on startup (optional)
-    ExtraText = function() return " nolagback" end -- text that goes next to the button in Text GUI (optional)
+    HoverText = "Disables anticheat", -- text that will show up after hovering over the button (optional)
+    Default = false, -- enabled on startup (optional)
+    ExtraText = function() return " CFrame" end -- text that goes next to the button in Text GUI (optional)
 })
 local RiseWaterMark = Utility.CreateOptionsButton({
     Name = "RiseWatermark", -- name of object
@@ -82,4 +111,45 @@ UIGradient.Parent = ImageLabel
     HoverText = "allows Infnite fly", -- text that will show up after hovering over the button (optional)
     Default = true, -- enabled on startup (optional)
     ExtraText = function() return " nolagback" end -- text that goes next to the button in Text GUI (optional)
+})
+runfunction()
+local function telToPlr(plrT)
+    local plr = plrT
+        local lplr = game.Players.LocalPlayer
+        task.wait()
+        plr.Character.HumanoidRootPart.Position = Vector3.new(plr.X, plr.Y, plr.Z)
+        return
+    end
+end
+local AutoWin2V2 = Utility.CreateOptionsButton({
+    Name = "AutoWin2V2", -- name of object
+    Function = function(callback) -- function that is called when toggled
+        if callback then
+            wait(0.05)
+            notify("AutoWin", "Teleporting To bed"), 8
+            wait(0.03)
+            lplr.Character:FindFirstChildOfClass("Humanoid"):ChangeState(Enum.HumanoidStateType.Dead)
+            lplr.CharacterAdded:Connect(function()
+                wait(0.3) 
+                tweenToNearestBed()
+                wait(4)
+                notify("AutoWin", "Killing Players", 2)
+            end)
+        end
+    end,
+    HoverText = "only works with 2v2 (scythe must be used)", -- text that will show up after hovering over the button (optional)
+    Default = false, -- enabled on startup (optional)
+})
+end
+local HostExploit = Blatant.CreateOptionsButton({
+    Name = "HostExploit", -- name of object
+    Function = function(callback) -- function that is called when toggled
+        if callback then
+            notify("HostExploit", "Giving Host", 1)
+            game.Players.LocalPlayer:SetAttribute("Cohost", true)
+            notify("HostExploit", "Client-Sided btw", 2)
+        end
+    end,
+    HoverText = "only works with 2v2 (scythe must be used)", -- text that will show up after hovering over the button (optional)
+    Default = false, -- enabled on startup (optional)
 })
